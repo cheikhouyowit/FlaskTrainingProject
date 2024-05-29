5d918a7e706546c9d87d538de3c0ea84a86a60f9
cheikhouyowit <113256574+yowitweb@users.noreply.github.com>
2024-05-29 14:05:59 +0000

    Add user authentication

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, UserMixin, login_user, current_user, logout_user
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = 'supersecretkey'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
     
     def __repr__(self):
         return f"User('{self.username}')"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
 def home():
@@ -32,12 +42,36 @@ def submit():
     </form>
     '''
 
-@app.route('/add_user')
-def add_user():
-    new_user = User(username="NewUser")
-    db.session.add(new_user)
-    db.session.commit()
-    return "User added!"
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        hashed_password = bcrypt.generate_password_hash(request.form['password']).decode('utf-8')
+        user = User(username=request.form['username'], password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        return 'User registered!'
+    return '''
+    <form method="POST">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="password" placeholder="Password">
+        <input type="submit">
+    </form>
+    '''
+
+@db.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = User.query.filter_by(username=request.form['username']).first()
+        if user and bcrypt.check_password_hash(user.password, request.form['password']):
+            login_user(user)
+            return 'Logged in!'
+    return '''
+    <form method="POST">
+        <input type="text" name="username" placeholder="Username":
+        <input type="password" name="password" placeholder="Password">
+        <input type="submit">
+    </form>
+    '''
 
 db.create_all()
 
