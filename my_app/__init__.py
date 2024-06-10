13e24ebb88a462db5eb7185dae82290ac28a536a
cheikhouyowit <113256574+yowitweb@users.noreply.github.com>
2024-06-10 12:27:07 +0000

    Update for sqlalchemy

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,9 +1,20 @@
 import ccy
 from flask import Flask, request
 from my_app.product.views import product_blueprint
+from my_app.catalog.views import catalog
+
+from flask import SQLAlchemy
 
 app = Flask(__name__)
 app.register_blueprint(product_blueprint)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+app.register_blueprint(catalog)
+
+db.create_all()
+
+
 
 
 @app.template_filter('format_currency')
